048cdb43c64fb9728fb33565acf8771111de19a7
gityongli <2508112607@qq.com>
2018-03-27 08:55:41 +0800

    修改1

--- js/index.js
+++ js/index.js
@@ -2,12 +2,4 @@
  * Created by ������ on 2017/8/30.
  */
 /*
-var imgSrc = new Array("images/bo1.jpg","images/bo2.jpeg","images/bo6.jpg")
-function sImg() {
-    var n = Math.abs(parseInt(Math.random() * 10) - 2);
-    if (n == 2) {
-        n = 1
-    }
-    document.getElementById("img1").src = imgSrc[n]
-}
-setInterval("sImg()", 1000);*/
+
